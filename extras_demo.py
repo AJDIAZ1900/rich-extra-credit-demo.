d9c4c10f50ced5d0b1821ce1b8526a06bdf77265
AJDIAZ1900 <ashley.diaz79@login.cuny.edu>
2025-09-28 22:10:11 -0400

    Create extras_demo.py

---
+++ extras_demo.py
@@ -0,0 +1,9 @@
+import emoji
+
+# Flags use regional indicators:
+print("US flag:", emoji.emojize(":United_States:"))
+print("DR flag:", emoji.emojize(":Dominican_Republic:"))
+
+# Skin tones (where supported by the shortcode)
+print("Thumbs up default:", emoji.emojize(":thumbs_up:"))
+print("Thumbs up medium skin tone:", emoji.emojize(":thumbs_up_medium_skin_tone:"))
