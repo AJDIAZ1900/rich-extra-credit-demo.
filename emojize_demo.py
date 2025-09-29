b940619de0ee937b10c6b40e16a8d15bd51aea7d
AJDIAZ1900 <ashley.diaz79@login.cuny.edu>
2025-09-28 22:09:37 -0400

    Update emojize_demo.py

--- emojize_demo.py
+++ emojize_demo.py
@@ -1,1 +1,9 @@
-emojize_demo.py
+import emoji
+
+samples = [
+    ":fire:", ":rocket:", ":thumbs_up:", ":clinking_beer_mugs:",
+    ":red_heart:", ":smiling_face_with_sunglasses:"
+]
+
+for s in samples:
+    print(f"{s} -> {emoji.emojize(s)}")
